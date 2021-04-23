b435e08d24d74735b2d199da654faa1ac78583a6
Manuel <manuel.m.studer@gmail.com>
2021-04-23 15:24:42 +0200

    resolving fixing seed

--- src/data/loader.py
+++ src/data/loader.py
@@ -57,14 +57,14 @@ class DataLoader_folds:
         self.shuffle = shuffle
         self.numFolds = numFolds
         self.kfold = StratifiedKFold(
-            n_splits=numFolds, shuffle=self.shuffle, random_state=42
+            n_splits=numFolds, shuffle=self.shuffle, random_state=seed
         )
 
 
 class DataLoader_split:
   """DataLoader to generate stratified training and test split"""
 
-  def __init__(self, csv_file, test_size=0.2, doStratify = True, random_state=42):
+  def __init__(self, csv_file, test_size=0.2, doStratify = True, random_state=seed):
     self.dataset = pd.read_csv(csv_file)
     self.x = self.dataset["sequences"]
     self.y = self.dataset["labels"]
@@ -72,10 +72,10 @@ class DataLoader_split:
     self.test_size = test_size
 
     if doStratify:
-      self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.x, self.y, test_size=test_size, stratify=self.y, random_state=random_state)
+      self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.x, self.y, test_size=test_size, stratify=self.y, random_state=seed)
 
     else:
-      self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.x, self.y, test_size=test_size, stratify=None, random_state=random_state)
+      self.train_x, self.test_x, self.train_y, self.test_y = train_test_split(self.x, self.y, test_size=test_size, stratify=None, random_state=seed)
 
 
 
