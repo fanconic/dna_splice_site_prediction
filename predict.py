c8e3d9206de4388bd0f6fbf56c3f7e87c01e4df0
serced <severin.husmann@hotmail.com>
2021-04-26 11:36:30 +0200

    deleted unnecssary torch import

--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import pandas as pd
-import torch.utils.data as data
 from src.data.preprocessing import (
     string_transform_labels,
     string_transform_onehot_char,
