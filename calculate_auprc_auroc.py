7ddd06c36e112b456b2ee499f8657bc7890361cd
fanconic <fanconic@gmail.com>
2021-04-27 13:28:57 +0200

    changed generation and calculation file

--- calculate_auprc_auroc.py
+++ calculate_auprc_auroc.py
@@ -8,74 +8,48 @@ from sklearn.metrics import (
     roc_curve,
 )
 from settings import *
-
-models = [
-    "K-Nearest Neighbours",
-    "Logistic Regression",
-    "Linear Support Vector Machine",
-    "Support Vector Machine",
-    "Gradient Boosting",
-    "MLP",
-    "Random Forest",
-    "SpliceAI80",
-    "SpliceAI400",
-]
+import pickle
 
 
-# Human Test set
-df_human_test_predictions = pd.read_csv(results_dir + "test_humans_results.csv")
-df_human_test_labels = pd.read_csv(data_path + "human_dna_test_split.csv")
-
-# C. elegans
-df_celegans_test_predictions = pd.read_csv(results_dir + "test_celegans_results.csv")
-df_celegans_test_labels = pd.read_csv(data_path + "C_elegans_test_seq.csv")
-
-# @ TA: you can uncomment the following lines and enter your path to the hidden human dataset
-# df_celegans_hidden_test_predictions = pd.read_csv(results_dir + "test_hidden_humans_results.csv")
-# df_celegans_hidde_test_labels = pd.read_csv(...)
-
-
-def print_results(models, ground_truth, df_preds):
+def calc_auprc_auroc(model, ground_truth, predict_probas):
     """Prints the AUPRC and AUROC values
     Args:
-        models: list of model names
+        model: model name
         ground_truth: array of true labels
-        df_preds: submission data frame with prediction probablilities
+        predict_probas: test set prediction probablities
     """
-    for model in models:
-        predict_probas = df_preds[model + "_preds"]
 
-        # here are the x, y scores for the AUPRC
-        precision, recall, _ = precision_recall_curve(ground_truth, predict_probas)
+    # here are the x, y scores for the AUPRC
+    precision, recall, _ = precision_recall_curve(ground_truth, predict_probas)
 
-        # here are the x, y scores for the AUROC
-        fpr, tpr, _ = roc_curve(ground_truth, predict_probas)
+    # here are the x, y scores for the AUROC
+    fpr, tpr, _ = roc_curve(ground_truth, predict_probas)
 
-        auprc_score = auc(recall, precision)
-        auroc_score = auc(fpr, tpr)
+    auprc_score = auc(recall, precision)
+    auroc_score = auc(fpr, tpr)
 
-        auprc_score_alt = average_precision_score(ground_truth, predict_probas)
-        auroc_score_alt = roc_auc_score(ground_truth, predict_probas)
+    auprc_score_alt = average_precision_score(ground_truth, predict_probas)
 
-        print("Model: {}".format(model))
-        print(
-            "AUPRC: {0:.4f},\tAverage Precision: {1:.4f},\tAUROC: {2:.4f},\tAUROC (alternative): {3:.4f}\n".format(
-                auprc_score, auprc_score_alt, auroc_score, auroc_score_alt
-            )
+    print("Model: {}".format(model))
+    print(
+        "AUPRC: {0:.4f},\tAverage Precision: {1:.4f},\tAUROC: {2:.4f}\n".format(
+            auprc_score, auprc_score_alt, auroc_score, 
         )
+    )
 
 
-print("Human Test Set")
-ground_truth = df_human_test_labels["labels"]
-print_results(models, ground_truth, df_human_test_predictions)
+df_results = pickle.load(open("results.pkl", "rb"))
 
-# @ TA: uncomment here to run the predicitons
-"""
-print("Human Hidden Test Set")
-ground_truth = df_celegans_hidde_test_labels["labels"]
-print_results(models, ground_truth, df_celegans_hidden_test_predictions)
-"""
+df_celegans = pd.read_csv(data_path + "C_elegans_test_seq.csv")
+df_humans = pd.read_csv(data_path + "human_dna_test_split.csv")
 
-print("C. Elegans Test Set")
-ground_truth = df_celegans_test_labels["labels"]
-print_results(models, ground_truth, df_celegans_test_predictions)
+for i in range(len(df_results)):
+    row = df_results.iloc[i]
+    data_name = row["input_data"]
+    model = row["model_name"]
+    if data_name == "C. elegans":
+        print("C. elegans:")
+        calc_auprc_auroc(model, df_celegans["labels"], row["test_predictions"])
+    else:
+        print("Human:")
+        calc_auprc_auroc(model, df_humans["labels"], row["test_predictions"])
--- generate_output.py
+++ generate_output.py
@@ -1,6 +1,52 @@
 import numpy as np
 import pandas as pd
 from settings import *
+from sklearn.metrics import (
+    average_precision_score,
+    auc,
+    precision_recall_curve,
+    roc_auc_score,
+    roc_curve,
+)
+import pickle
+
+
+def calc_auprc_auroc(model, ground_truth, predict_probas):
+    """Prints the AUPRC and AUROC values
+    Args:
+        model: model name
+        ground_truth: array of true labels
+        predict_probas: test set prediction probablities
+    Returns:
+        auprc_score: AUPRC score
+        auroc_score: AUROC score
+        precision: precision values for AUPRC score
+        recall: recall values for AUPRC score
+        fpr: fpr values for AUROC score
+        tpr: tpr values for AUROC score
+    """
+
+    # here are the x, y scores for the AUPRC
+    precision, recall, _ = precision_recall_curve(ground_truth, predict_probas)
+
+    # here are the x, y scores for the AUROC
+    fpr, tpr, _ = roc_curve(ground_truth, predict_probas)
+
+    auprc_score = auc(recall, precision)
+    auroc_score = auc(fpr, tpr)
+
+    auprc_score_alt = average_precision_score(ground_truth, predict_probas)
+    auroc_score_alt = roc_auc_score(ground_truth, predict_probas)
+
+    print("Model: {}".format(model))
+    print(
+        "AUPRC: {0:.4f},\tAverage Precision: {1:.4f},\tAUROC: {2:.4f},\tAUROC (alternative): {3:.4f}\n".format(
+            auprc_score, auprc_score_alt, auroc_score, auroc_score_alt
+        )
+    )
+
+    return auprc_score, auroc_score, precision, recall, fpr, tpr
+
 
 models = [
     "K-Nearest Neighbours",
@@ -14,26 +60,53 @@ models = [
     "SpliceAI400",
 ]
 
-# celegans results
-df = pd.read_csv(data_path + "C_elegans_test_seq.csv")
-df = df.drop(["labels"], axis=1)
-df = df.rename(columns={"Unnamed: 0": "original_index"})
+data_names = ["C. elegans", "Human"]
+
+columns = [
+    "model_name",
+    "input_data",
+    "AUROC",
+    "AUPRC",
+    "test_predictions",
+    "hidden_test_predictions",
+    "fpr",
+    "tpr",
+    "precision",
+    "recall",
+]
+df_result = pd.DataFrame(columns=columns)
+
+df_celegans = pd.read_csv(data_path + "C_elegans_test_seq.csv")
+df_humans = pd.read_csv(data_path + "human_dna_test_split.csv")
 
 for model in models:
-    probas = np.load(results_dir + model + "_celegans_probas.npy")
-    df[model + "_preds"] = probas.flatten()
-df.to_csv(results_dir + "test_celegans_results.csv")
+    for data_name in data_names:
+        if data_name == "C. elegans":
+            preds = np.load(results_dir + model + "_celegans_probas.npy").flatten()
+            preds_hidden = np.nan
+            auprc_score, auroc_score, precision, recall, fpr, tpr = calc_auprc_auroc(
+                model, df_celegans["labels"], preds
+            )
+        else:
+            preds = np.load(results_dir + model + "_humans_probas_test.npy").flatten()
+            preds_hidden = np.load(results_dir + model + "_humans_probas.npy").flatten()
+            auprc_score, auroc_score, precision, recall, fpr, tpr = calc_auprc_auroc(
+                model, df_humans["labels"], preds
+            )
 
-# humans hidden
-df = pd.read_csv(data_path + "human_dna_test_hidden_split.csv")
-for model in models:
-    probas = np.load(results_dir + model + "_humans_probas.npy")
-    df[model + "_preds"] = probas.flatten()
-df.to_csv(results_dir + "test_hidden_humans_results.csv")
+        new_row = {
+            "model_name": model,
+            "input_data": data_name,
+            "AUROC": auroc_score,
+            "AUPRC": auprc_score,
+            "test_predictions": preds,
+            "hidden_test_predictions": preds_hidden,
+            "fpr": fpr,
+            "tpr": tpr,
+            "precision": precision,
+            "recall": recall,
+        }
 
-# humans test
-df = pd.read_csv(data_path + "human_dna_test_split.csv")
-for model in models:
-    probas = np.load(results_dir + model + "_humans_probas_test.npy")
-    df[model + "_preds"] = probas.flatten()
-df.to_csv(results_dir + "test_humans_results.csv")
+        df_result = df_result.append(new_row, ignore_index=True)
+
+pickle.dump(df_result, open("results.pkl", "wb"))
